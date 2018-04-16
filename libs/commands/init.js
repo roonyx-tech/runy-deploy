1e62b736654734a971378ea85c922a98fdf90f53
d4dd4r <prokhor2253@gmail.com>
2018-04-16 11:56:46 +0300

    updated init command

--- libs/commands/init.js
+++ libs/commands/init.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const fs = require('fs');
+const config = require(`${__dirname}/../../config`);
+
 const init = () => {
-  console.log('init works');
+  fs.copyFile(`${__dirname}/../config.conf.js`, `${process.cwd()}/${config.configName}`, err => {
+    if (err) throw err;
+    console.log(`${config.configName} was successfully created`);
+  });
 };
 
 module.exports = {
